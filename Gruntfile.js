cdc0c69a56d4766beee14c50c38a889b2d2d2944
jixinchan <1209980074@qq.com>
2019-03-12 15:07:50 +0800

    imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,18 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    htmlmin: {
-      options: {
-        collapseWhitespace: true,
-        preserveLineBreaks: false
-      },
-      files: {
-        src: './index.html',
-        dest: 'dist/index.html'
-      }
-      
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: './images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/'                                                      }]
+      }      
     }
              
   });
 
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.registerTask('default', ['htmlmin']);
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['imagemin']);
 };
 
