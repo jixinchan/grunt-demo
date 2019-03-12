176361e8c31669da2c6863e5ffceacc90aae2a5d
jixinchan <1209980074@qq.com>
2019-03-12 15:39:13 +0800

    sprite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,14 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    concat: {
-      options: {
-        separator: ';',
-      },
-      dist: {
-        src: ['rectangle.js', 'calc.js'],
-        dest: 'dist/bundle.js',
-      },
+    sprite: {
+      all: {
+        src: './images/*.png',
+        dest:'./images/allInOne.png',
+        destCss:'./sprites.css'                               }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.registerTask('default', ['sprite']);
 };
 
